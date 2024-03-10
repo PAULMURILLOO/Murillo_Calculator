3acd21be4dee851bc5d58eb894e09680ea8b8a34
PAULMURILLOO <pauljomarmurillo@gmail.com>
2024-03-10 19:08:17 +0800

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Murillo_Calculator_Final"
+include(":app")
+ 
